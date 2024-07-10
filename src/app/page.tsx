68f1c265023bddd85747918cb58c5a647fa82a55
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-07-11 00:50:34 +0900

    feat : route test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,34 @@
 import { css } from '@emotion/react';
 
 const Home = () => {
-  return <div css={test}>Hello</div>;
+  return (
+    <div css={test}>
+      <div css={divStyle}>
+        <a css={aStyle} href="/test">
+          test
+        </a>
+      </div>
+      <div css={divStyle}>
+        <a css={aStyle} href="/test/order">
+          order
+        </a>
+      </div>
+    </div>
+  );
 };
 const test = css`
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
   background-color: blue;
+  height: 100vh;
+  font-size: 40px;
+`;
+const divStyle = css`
+  width: 50%;
+  border: 1px solid black;
+`;
+const aStyle = css`
+  color: #d831ca;
 `;
 export default Home;
