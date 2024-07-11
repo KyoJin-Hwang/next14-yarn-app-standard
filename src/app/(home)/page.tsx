f04391154040c58aa1791004db0739683669e87c
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-07-11 14:41:16 +0900

    fix : div 삭제

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,16 +8,13 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div css={test}>
-      <div css={divStyle}>
-        <Link css={aStyle} href="/test">
-          test
-        </Link>
-      </div>
-      <div css={divStyle}>
-        <Link css={aStyle} href="/test/order">
-          order
-        </Link>
-      </div>
+      <Link css={aStyle} href="/test">
+        test
+      </Link>
+
+      <Link css={aStyle} href="/test/order">
+        order
+      </Link>
     </div>
   );
 };
@@ -25,15 +22,13 @@ const test = css`
   display: flex;
   gap: 20px;
   flex-direction: column;
-  background-color: blue;
+
   height: 100vh;
   font-size: 40px;
 `;
-const divStyle = css`
-  width: 50%;
-  border: 1px solid black;
-`;
+
 const aStyle = css`
+  display: inline-block;
   color: #d831ca;
 `;
 export default Home;
